56794485f0a3ee44fa371aba968784f410e709dc
yyh1102 <234347589@qq.cim>
2017-04-29 21:03:20 +0800

    modify deploy command line

--- autoDeploy/autoDeploy.sh
+++ autoDeploy/autoDeploy.sh
@@ -6,7 +6,6 @@ echo "pulling source code..."
 git pull origin master
 git checkout master
 echo "Pull finished"
-npm run build
+npm run deploy
 echo "build completed"
-pm2 reload lowesnews
 echo "pm2 reload completed"
